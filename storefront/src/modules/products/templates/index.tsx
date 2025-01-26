b96f1b9445d08cd259e2c3bdc72011e1b7c78a92
ruch777 <rshashanka@gmail.com>
2025-01-26 20:09:06 +0530

    product update

--- storefront/src/modules/products/templates/index.tsx
+++ storefront/src/modules/products/templates/index.tsx
@@ -29,29 +29,31 @@ const ProductTemplate: React.FC<ProductTemplateProps> = ({
   return (
     <>
       <div
-        className="content-container flex flex-col small:flex-row small:items-start py-6 relative"
+        className="content-container flex flex-col lg:flex-row lg:items-start py-6 relative"
         data-testid="product-container"
       >
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-6">
-          <ProductInfo product={product} />
-          <ProductTabs product={product} />
-        </div>
-        <div className="block w-full relative">
-          <ImageGallery images={product?.images || []} />
-        </div>
-        <div className="flex flex-col small:sticky small:top-48 small:py-0 small:max-w-[300px] w-full py-8 gap-y-12">
-          <ProductOnboardingCta />
-          <Suspense
-            fallback={
-              <ProductActions
-                disabled={true}
-                product={product}
-                region={region}
-              />
-            }
-          >
-            <ProductActionsWrapper id={product.id} region={region} />
-          </Suspense>
+        <div className="flex flex-col lg:flex-row w-full">
+          <div className="flex flex-col lg:sticky lg:top-48 lg:py-0 w-full lg:w-1/2 py-8 gap-y-6">
+            <ProductInfo product={product} />
+            <ProductTabs product={product} />
+          </div>
+          <div className="block w-full lg:w-1/2 relative">
+            <ImageGallery images={product?.images || []} />
+          </div>
+          <div className="flex flex-col lg:sticky lg:top-48 lg:py-0 w-full lg:w-1/2 py-8 gap-y-12">
+            <ProductOnboardingCta />
+            <Suspense
+              fallback={
+                <ProductActions
+                  disabled={true}
+                  product={product}
+                  region={region}
+                />
+              }
+            >
+              <ProductActionsWrapper id={product.id} region={region} />
+            </Suspense>
+          </div>
         </div>
       </div>
       <div
